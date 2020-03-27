e9e6ec910bdf28b014cf30024e7883113ec739b3
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-03-27 13:11:33 +0100

    absolute path and dynamic path added in app-route with tabs view on places page

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -5,21 +5,68 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./discover/discover.module').then(
+                m => m.DiscoverPageModule
+              )
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import('./discover/place-detail/place-detail.module').then(
+                m => m.PlaceDetailPageModule
+              )
+          },
+          {
+            path: '',
+            redirectTo: 'places/tabs/discover',
+            pathMatch: 'full'
+          }
+        ]
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./offers/offers.module').then(m => m.OffersPageModule)
+          },
+          {
+            path: 'new-offer',
+            loadChildren: () => import('./offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () => import('./offers/edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then(m => m.OfferBookingsPageModule)
+          },
+        ]
+      }
+    ]
+  },
+  {
     path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
   }
 ];
 
+// hard coded routes first before dynamic routes
+// remember that tab name id in html must be the same as the route path and route with :productId is a dynamic param
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class PlacesPageRoutingModule {}
