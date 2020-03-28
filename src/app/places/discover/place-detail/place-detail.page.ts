3f8bce60c72dc08596d45f93652d971d4e8bf740
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-03-28 17:09:27 +0100

    adding back button using router,navCtrl and pop to navigate

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
-
-  constructor() { }
+  // WE INJECT NAV CONTROLLER TO HELP AID PROPER PAGE TRANSITION
+  constructor(private router: Router, private navCrtl: NavController) { }
 
   ngOnInit() {
+  }
+
+  onBookPlace() {
+    // THE BELOW IS NAVIGATING USING navigateByUrl
+    // this.router.navigateByUrl('/places/tabs/discover');
+
+    // UNDER THE HTMLIonModalElement, THIS USED ANGULAR ROUTER
+    this.navCrtl.navigateBack('/places/tabs/discover');
+
+    // navigate using the this.navCrtl which will then pop the last screen on the page
+    // this.navCrtl.pop();
   }
 
 }
