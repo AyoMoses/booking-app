c74678d452d383ba9c8f6e029ff296cb25d9bf1d
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-01 13:58:18 +0100

    added place detail on navigation

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -22,15 +22,10 @@ export class PlaceDetailPage implements OnInit {
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
-      // If we do not have a matching place ID then we navigate back to offers
-      // THE BELOW EXTRACTS ID OF LOADED PLACE
       if (!paramMap.has('placeId')) {
         this.navCtrl.navigateBack('/places/tabs/offers');
-        // ADD RETURN SO OTHER CODE DOES NOT GET EXECUTED
         return;
       }
-      // IF WE HAVE A PLACE ID THEN WE LOAD THE PLACE id
-      // LOAD THE PLACES THEN FIND OUR FITTING PLACE AS ITS GOTTEN FROM THE GETPLACE SERVICE FUNCTION CREATED
       this.place = this.placesService.getPlace(paramMap.get('placeId'));
     });
   }
