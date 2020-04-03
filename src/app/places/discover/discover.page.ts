90daf21e5473473d3e7fe2dab5f421769b36b9cf
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-03 18:48:05 +0100

    virtual scrolling added to discover page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -8,12 +8,16 @@ import { Place } from '../place.model';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
+  // Below is our place model
   loadedPlaces: Place[];
+  // below is displaying our list dynamically when we use slice() to output from the first list
+  listedLoadedPlaces: Place[];
 
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 
 }
