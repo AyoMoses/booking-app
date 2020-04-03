4adc39dc977238fc409dee3ae4ef9730312710dd
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-03 16:00:56 +0100

    ion-item-sliding added to ion-list in offers page to enable sliding to option for edit page

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
@@ -8,12 +11,21 @@ import { PlacesService } from '../places.service';
   styleUrls: ['./offers.page.scss'],
 })
 export class OffersPage implements OnInit {
+  // load the place-model
   loadedOffers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
+    // inject the this.placesService to get data
     this.loadedOffers = this.placesService.places;
   }
 
+  // IMPORT SLIDING ITEM AND CLOSE WHEN CLICKED THEN NAVIGATING TO EDIT BY GETTING THE OFFER ID
+  // THIS METHOD ENSURES THE SLIDER IS CLOSED AND REMAINS CLOSEED WHEN NAVIGATED BACK
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log('Editing item', offerId);
+  }
 }
