7adb3c63ef0bd2702784259a7ddc9d18790cc3b9
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-05 14:37:31 +0100

    new-offer reactive validation

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -6,13 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
+  // create form property
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    // we add controls of what we intend to control in out forms
+    this.form = new FormGroup({
+      // each form control takes an object to configure them. null is no default value
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
   }
 
   onCreateOffers() {
-    console.log('creating offered place...');
+    // test forms like this on console
+    console.log(this.form);
   }
 }
