5e80b87f7eb84cf450d450d6ebc26ee9547768dd
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-11 20:07:00 +0100

    displaying our google map

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
 
 import { ModalController } from '@ionic/angular';
 
@@ -7,12 +14,60 @@ import { ModalController } from '@ionic/angular';
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent implements OnInit {
-  constructor(private modalCtrl: ModalController) {}
+export class MapModalComponent implements OnInit, AfterViewInit {
+  // @ViewChild('map') mapElementRef: ElementRef;
+
+  @ViewChild('map', { static: true }) mapElementRef: ElementRef<HTMLDivElement>;
+
+  constructor(private modalCtrl: ModalController, private renderer: Renderer2) {}
 
   ngOnInit() {}
 
+  // IN CASE THE REJECT PROMISE FAILS TO LOAD THE MAP, WE THEN CATCH THE ERROR
+  ngAfterViewInit() {
+    this.getGoogleMaps()
+      .then((googleMaps) => {
+        const mapEl = this.mapElementRef.nativeElement;
+        const map = new googleMaps.Map(mapEl, {
+          center: { lat: 6.618132, lng: 3.429364 },
+          zoom: 16,
+        });
+
+        // LISTEN TO IT ONCE AFTER THE FIRST INITIAL LOAD. renderer is used to manipulate our dom
+        googleMaps.event.addListenerOnce(map, 'idle', () => {
+          this.renderer.addClass(mapEl, 'visible');
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   onCancel() {
     this.modalCtrl.dismiss();
+  }
+
+  private getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+    if (googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
+    }
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src =
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyCnAB_VM1R7sMyl-Dv1YebR4rwa29GG6uA';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google maps SDK not available.');
+        }
+      };
+    });
   }
 }
