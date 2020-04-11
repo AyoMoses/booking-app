e6e4a85fc1aea64d00a328264e3652996bb74059
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-11 21:07:19 +0100

    location of a place

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -9,6 +9,8 @@ import {
 
 import { ModalController } from '@ionic/angular';
 
+import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
@@ -56,6 +58,7 @@ export class MapModalComponent implements OnInit, AfterViewInit {
     this.modalCtrl.dismiss();
   }
 
+  // GOOGLE MAPS SDK
   private getGoogleMaps(): Promise<any> {
     const win = window as any;
     const googleModule = win.google;
@@ -65,7 +68,7 @@ export class MapModalComponent implements OnInit, AfterViewInit {
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
       script.src =
-        'https://maps.googleapis.com/maps/api/js?key=AIzaSyCnAB_VM1R7sMyl-Dv1YebR4rwa29GG6uA';
+        'https://maps.googleapis.com/maps/api/js?key=' + environment.googleMapsAPIKey;
       script.async = true;
       script.defer = true;
       document.body.appendChild(script);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  googleMapsAPIKey: 'AIzaSyCnAB_VM1R7sMyl-Dv1YebR4rwa29GG6uA'
 };
