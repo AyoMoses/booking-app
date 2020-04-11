7c7e89521b3e0654fd6fd8d21f733355a319b665
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-11 22:11:28 +0100

    minimal optimization to destroy click listener once location has been picked

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -5,21 +5,27 @@ import {
   ViewChild,
   ElementRef,
   Renderer2,
+  OnDestroy,
 } from '@angular/core';
 
 import { ModalController } from '@ionic/angular';
 
 import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent implements OnInit, AfterViewInit {
+export class MapModalComponent implements OnInit, AfterViewInit, OnDestroy {
   // @ViewChild('map') mapElementRef: ElementRef;
 
   @ViewChild('map', { static: true }) mapElementRef: ElementRef<HTMLDivElement>;
+
+  // STORE THE BELOW IN A GLOBAL VARIABLE
+  clickListener: any;
+  googleMaps: any;
 
   constructor(private modalCtrl: ModalController, private renderer: Renderer2) {}
 
@@ -29,19 +35,21 @@ export class MapModalComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.getGoogleMaps()
       .then((googleMaps) => {
+        this.googleMaps = googleMaps;
         const mapEl = this.mapElementRef.nativeElement;
+        // tslint:disable-next-line: no-shadowed-variable
         const map = new googleMaps.Map(mapEl, {
           center: { lat: 6.618132, lng: 3.429364 },
           zoom: 16,
         });
 
         // LISTEN TO IT ONCE AFTER THE FIRST INITIAL LOAD. renderer is used to manipulate our dom
-        googleMaps.event.addListenerOnce(map, 'idle', () => {
+        this.googleMaps.event.addListenerOnce(map, 'idle', () => {
           this.renderer.addClass(mapEl, 'visible');
         });
 
         // ADD EVENT LISTENER ON THE MAP
-        map.addListener('click', event => {
+        this.clickListener = map.addListener('click', event => {
           const selectedCoords = {
             lat: event.latLng.lat(),
             lng: event.latLng.lng()
@@ -56,6 +64,12 @@ export class MapModalComponent implements OnInit, AfterViewInit {
 
   onCancel() {
     this.modalCtrl.dismiss();
+  }
+
+  // this ensure when we dismiss the modal and clear the map, we actually get rid of click listener
+  // so we do not introduce a memory leak
+  ngOnDestroy() {
+    this.googleMaps.event.removeListener(this.clickListener);
   }
 
   // GOOGLE MAPS SDK
