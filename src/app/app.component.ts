a0476b1712eeed2a8b86851ded0b5a2c125cc256
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-14 11:26:14 +0100

    capacitor enabled in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins, Capacitor } from '@capacitor/core';
 
 import { AuthService } from './auth/auth.service';
 
@@ -15,8 +14,6 @@ import { AuthService } from './auth/auth.service';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
     private authService: AuthService,
     private router: Router
   ) {
@@ -24,9 +21,12 @@ export class AppComponent {
   }
 
   initializeApp() {
+    // this.platform.is('ios'); this is good to check on run codes on certain platforms
+    // you can add console log to check with check returns true console.log(this.platform.is('ios'));
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (Capacitor.isPluginAvailable('SplashScreen')) {
+        Plugins.SplashScreen.hide();
+      }
     });
   }
 
