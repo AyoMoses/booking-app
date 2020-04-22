a33b3633a318dcd8e510d3b54beb0a90bacc63b7
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-22 13:58:42 +0100

    users autolocate added

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -60,12 +60,13 @@ export class LocationPickerComponent implements OnInit {
       });
   }
 
-  // AUTO LOCATE USER
+  // AUTO LOCATE USER COORDINATES
   private locateUser() {
     if (!Capacitor.isPluginAvailable('Geolocation')) {
       this.showErrorAlert();
       return; // return means to stop and not execute next set of code
     }
+    this.isLoading = true;
     // IF WE MAKE IT THRU THE ABOVE CODE WITHOUT AN ERRO
     Plugins.Geolocation.getCurrentPosition()
       .then((geoPosition) => {
@@ -73,8 +74,11 @@ export class LocationPickerComponent implements OnInit {
           lat: geoPosition.coords.latitude,
           lng: geoPosition.coords.longitude,
         };
+        this.createPlace(coordinates.lat, coordinates.lng);
+        this.isLoading = false;
       })
       .catch((err) => {
+        this.isLoading = false;
         this.showErrorAlert();
       });
   }
@@ -84,6 +88,7 @@ export class LocationPickerComponent implements OnInit {
       .create({
         header: 'Could not fetch location',
         message: 'Please use the map to pick a location!',
+        buttons: ['Okay']
       })
       .then((alertEl) => {
         alertEl.present();
@@ -96,34 +101,45 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return;
         }
-        const pickedLocation: PlaceLocation = {
+        const coordinates: Coordinates = {
           lat: modalData.data.lat,
           lng: modalData.data.lng,
-          address: null,
-          staticMapImageUrl: null,
         };
-        // LOAD SPINNER ONCE WE ARE DONE PICKING AN ADDRESS
-        this.isLoading = true;
-        this.getAddress(modalData.data.lat, modalData.data.lng)
-          .pipe(
-            switchMap((address) => {
-              pickedLocation.address = address;
-              // of is wrapped around any value that is instantly emmited
-              return of(
-                this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
-              );
-            })
-          )
-          .subscribe((staticMapImageUrl) => {
-            pickedLocation.staticMapImageUrl = staticMapImageUrl;
-            this.selectedLocationImage = staticMapImageUrl;
-            // LOADING IS FALSE ONCE WE GET OUR IMAGE
-            this.isLoading = false;
-            this.locationPick.emit(pickedLocation);
-          });
+        this.createPlace(coordinates.lat, coordinates.lng);
       });
       modalEl.present();
     });
+  }
+
+  private createPlace(lat: number, lng: number) {
+    const pickedLocation: PlaceLocation = {
+      // tslint:disable-next-line: object-literal-shorthand
+      lat: lat,
+      // tslint:disable-next-line: object-literal-shorthand
+      lng: lng,
+      address: null,
+      staticMapImageUrl: null,
+    };
+
+    // LOAD SPINNER ONCE WE ARE DONE PICKING AN ADDRESS
+    this.isLoading = true;
+    this.getAddress(lat, lng)
+      .pipe(
+        switchMap((address) => {
+          pickedLocation.address = address;
+          // of is wrapped around any value that is instantly emmited
+          return of(
+            this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
+          );
+        })
+      )
+      .subscribe((staticMapImageUrl) => {
+        pickedLocation.staticMapImageUrl = staticMapImageUrl;
+        this.selectedLocationImage = staticMapImageUrl;
+        // LOADING IS FALSE ONCE WE GET OUR IMAGE
+        this.isLoading = false;
+        this.locationPick.emit(pickedLocation);
+      });
   }
 
   private getAddress(lat: number, lng: number) {
