ea271c87fc679fa126756f94109d8975233610ba
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2020-04-23 12:16:21 +0100

    picking image and displaying it on desktop and desktop mobile view

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,5 +1,17 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Plugins, Capacitor, CameraSource, CameraResultType } from '@capacitor/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import {
+  Plugins,
+  Capacitor,
+  CameraSource,
+  CameraResultType,
+} from '@capacitor/core';
 import { Platform } from '@ionic/angular';
 
 @Component({
@@ -8,10 +20,15 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['./image-picker.component.scss'],
 })
 export class ImagePickerComponent implements OnInit {
+  // had to change static to false cos of change detection in angular 8
+  @ViewChild('filePicker', { static: false }) filePickerRef: ElementRef<
+    HTMLInputElement
+  >;
   @Output() imagePick = new EventEmitter<string>();
   selectedImage: string;
+  usePicker = false;
 
-  constructor(private platform: Platform) { }
+  constructor(private platform: Platform) {}
 
   ngOnInit() {
     console.log('Mobile:', this.platform.is('mobile'));
@@ -19,11 +36,20 @@ export class ImagePickerComponent implements OnInit {
     console.log('iOS:', this.platform.is('ios'));
     console.log('Android:', this.platform.is('android'));
     console.log('Desktop:', this.platform.is('desktop'));
+
+    // check if we are previewing on desktop mode or desktop mobile mode
+    if (
+      (this.platform.is('mobile') && !this.platform.is('hybrid')) ||
+      this.platform.is('desktop')
+    ) {
+      this.usePicker = true;
+    }
   }
 
   onPickImage() {
-    if (!Capacitor.isPluginAvailable('Camera')) {
-
+    if (!Capacitor.isPluginAvailable('Camera') || this.usePicker) {
+      // this.filePickerRef.nativeElement.click();
+      this.filePickerRef.nativeElement.click();
       return;
     }
     Plugins.Camera.getPhoto({
@@ -32,13 +58,29 @@ export class ImagePickerComponent implements OnInit {
       correctOrientation: true,
       height: 320,
       width: 200,
-      resultType: CameraResultType.Base64
-    }).then(image => {
-      this.selectedImage = image.base64String;
-      this.imagePick.emit(image.base64String);
-    }).catch(error => {
-      console.log(error);
-      return false;
-    });
+      resultType: CameraResultType.Base64,
+    })
+      .then((image) => {
+        this.selectedImage = image.base64String;
+        this.imagePick.emit(image.base64String);
+      })
+      .catch((error) => {
+        console.log(error);
+        return false;
+      });
+  }
+
+  onFileChosen(event: Event) {
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+    if (!pickedFile) {
+      return;
+    }
+    // having picked a file we then display. FILE READER
+    const fr = new FileReader();
+    fr.onload = () => {
+      const dataUrl = fr.result.toString();
+      this.selectedImage = dataUrl;
+    };
+    fr.readAsDataURL(pickedFile);
   }
 }
